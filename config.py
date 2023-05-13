5b7016ebce3c9f0e09c898c953c179e22e38db4b
ICC TEAM <131507368+GOVIND-BOTS@users.noreply.github.com>
2023-05-13 20:38:14 +0530

    config.py

--- config.py
+++ config.py
@@ -7,20 +7,20 @@ load_dotenv()
 
 # FOR CODES
 
-API_ID = int(getenv("API_ID")) 
-API_HASH = getenv("API_HASH") 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5391883908").split()))
-LOGGER = int(getenv("LOGGER", None)) 
-OWNER = int(getenv("OWNER_ID")) 
+API_ID = int(getenv("API_ID", "27424332"))
+API_HASH = getenv("API_HASH", "cb93e76ed8e78c8081f52cd3aa66f08b")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5348648456").split()))
+LOGGER = int(getenv("LOGGER", "-1001948541213")
+OWNER = int(getenv("OWNER_ID", "6099950428"))
 NAME = getenv("ALIVE_NAME")
-OWN_USERNAME= getenv("OWN_USERNAME")
-ALIVE_PIC = getenv("ALIVE_PIC") 
+OWN_USERNAME= getenv("OWN_USERNAME", "@Ankit_Varma42")
+ALIVE_PIC = getenv("ALIVE_PIC", "https://te.legra.ph/file/e423cd7f9c3ce51d48d20.jpg")
 
 # FOR SPAMBOT
 
-TOKEN1 = getenv("TOKEN1", None) 
-TOKEN2 = getenv("TOKEN2", None) 
-TOKEN3 = getenv("TOKEN3", None) 
+TOKEN1 = getenv("TOKEN1", "6032040315:AAFyBtAEBGf3RDgl-DjSiQUejFexmeDDnDI") 
+TOKEN2 = getenv("TOKEN2", "6087616147:AAHYk5umnz10lgJpRs6-IvGbuUEfXh_RR5E") 
+TOKEN3 = getenv("TOKEN3", NOne) 
 TOKEN4 = getenv("TOKEN4", None) 
 TOKEN5 = getenv("TOKEN5", None) 
 TOKEN6 = getenv("TOKEN6", None) 
